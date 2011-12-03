0e1c26ca99ff73d4f56eb10aea2c2121f51d9f07
Ian Dees <ian.dees@gmail.com>
2011-12-02 18:07:03 -0800

    Damn PHP and your Null==''!

--- osm_out.php
+++ osm_out.php
@@ -1,30 +1,36 @@
 <?
 
 function color($prev, $curr) {
-  if($prev == "" and $curr == "") {
+  if($prev == "----" and $curr == "----") {
     return "notpresent";
-  } else if($prev == "" and $curr != "") {
+  } else if($prev == "----" and $curr != "----") {
     return "new";
-  } else if($prev != "" and $curr != "") {
+  } else if($prev != "----" and $curr != "----") {
     if($prev == $curr) {
       return "unchanged";
     } else {
       return "changed";
     }
-  } else if($prev != "" and $curr == "") {
+  } else if($prev != "----" and $curr == "----") {
     return "removed";
   }
 }
 
 function refLine($ways, $ref) {
   $ret = "<tr>\n";
-  $previousVal = "";
+  $previousVal = "----";
   $i = 0;
   
   $ret .= "<td style='background:#ccc;'><a href='node.php?id=$ref'>$ref</a></td>";
 
   foreach ($ways as $way) {
-    $currentVal = $way['refs'][$ref];
+    $refs = $way['refs'];
+    if(!array_key_exists($ref, $refs)) {
+      $currentVal = "----";
+      $ret .= "<td class='$class'>&nbsp;</td>";
+    } else {
+      $currentVal = $ref[$ref];
+    }
     $class = color($previousVal, $currentVal);
     $ret .= "<td class='$class'>&nbsp;</td>";
     $previousVal = $currentVal;
@@ -33,23 +39,57 @@ function refLine($ways, $ref) {
   return $ret;
 }
 
+function memberLine($relations, $type, $ref) {
+  $ret = "<tr>";
+  $previousVal = "----";
+  $i = 0;
+
+  $ret .= "<td style='background:#ccc;'><img src='$type.png'/><a href='$type.php?id=$ref'>$ref</a></td>";
+
+  foreach ($relations as $relation) {
+    $members = $relation['members'];
+    if(!array_key_exists("$type,$ref", $members)) {
+      $currentVal = "----";
+      $class = color($previousVal, $currentVal);
+      $ret .= "<td class='$class'>&nbsp;</td>";
+    } else {
+      $currentVal = $members["$type,$ref"];
+      $class = color($previousVal, $currentVal);
+      if($currentVal == "") {
+        $ret .= "<td class='$class empty'>(empty)</td>";
+      } else {
+        $ret .= "<td class='$class'>$currentVal</td>";
+      }
+    }
+    $previousVal = $currentVal;
+  }
+  $ret .= "</tr>\n";
+  return $ret;
+}
 
 function tagLine($ways, $key, $title) {
   $ret = "<tr>\n";
-  $previousVal = "";
+  $previousVal = "----";
   $i = 0;
   
   $ret .= "<td style='background:#ccc;'>$title</td>";
 
   foreach ($ways as $way) {
-    $currentVal = $way['tags'][$key];
-    $shortVal = $currentVal;
-    $class = color($previousVal, $currentVal);
-    if(strlen($currentVal) > 20) {
-      $shortVal = substr($currentVal, 0, 20) . "&#8230;";
-      $ret .= "<td class='$class'><abbr title='$currentVal'>$shortVal</abbr></td>";
+    $tags = $way['tags'];
+    if(!array_key_exists($key, $tags)) {
+      $currentVal = "----";
+      $class = color($previousVal, $currentVal);
+      $ret .= "<td class='$class'>&nbsp;</td>";
     } else {
-      $ret .= "<td class='$class'>$currentVal</td>";
+      $currentVal = $way['tags'][$key];
+      $shortVal = $currentVal;
+      $class = color($previousVal, $currentVal);
+      if(strlen($currentVal) > 20) {
+        $shortVal = substr($currentVal, 0, 20) . "&#8230;";
+        $ret .= "<td class='$class'><abbr title='$currentVal'>$shortVal</abbr></td>";
+      } else {
+        $ret .= "<td class='$class'>$currentVal</td>";
+      }
     }
     $previousVal = $currentVal;
   }
@@ -70,7 +110,7 @@ function timeLine($ways) {
 
 function wayLine($ways, $val, $useColor=true, $title) {
   $ret = "<tr>\n";
-  $previousVal = "";
+  $previousVal = "----";
   $ret .= "<td style='background:#ccc;'>$title</td>";
   foreach ($ways as $way) {
     $currentVal = $way[$val];
