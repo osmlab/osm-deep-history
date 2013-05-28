eb7d1c2f35d9bc8ad2905df9d6fef1943d768b61
Ian Dees <ian.dees@gmail.com>
2013-05-27 20:06:33 -0500

    Use a helper function for each line and make it change color.

--- js/site.js
+++ js/site.js
@@ -17,6 +17,33 @@ $(document).ready(function() {
     }
 });
 
+var dataColumn = function(row_name, data, key, value_transform) {
+    var transform = value_transform || function(keyval) { return keyval; };
+    var prev = null;
+
+    var html = "<tr><td class='first_column'>" + row_name + "</td>";
+    for (var i = 0; i < data.length; i++) {
+        var val = transform(data[i][key]);
+
+        var clazz = '';
+        if (prev == val) {
+            clazz = 'unchanged';
+        } else if (!prev && val && val != prev) {
+            clazz = 'added';
+        } else if (prev && !val) {
+            clazz = 'removed';
+        } else if (val && prev != val) {
+            clazz = 'changed';
+        }
+
+        html += "<td class='" + clazz + "'>" + val + "</td>";
+        prev = val;
+    }
+    html += "</tr>";
+
+    return html;
+};
+
 $('#go').click(function() {
     var type = $('#type').val(),
         id = +$('#id').val();
@@ -32,35 +59,18 @@ $('#go').click(function() {
 
         console.log(objects);
 
-        html += "<tr><th class='first_column'>Version</th>";
+        html += "<tr class='row_header'><th class='first_column'>Version</th>";
         for (i = 0; i < object.length; i++) {
             step = object[i];
             html += "<th>" + step.version + "</th>";
         }
         html += "</tr>";
 
-        html += "<tr><td class='first_column'>Time</td>";
-        for (i = 0; i < object.length; i++) {
-            step = object[i];
-            html += "<td>" + step.timestamp.format("LLL") + "</td>";
-        }
-        html += "</tr>";
+        html += dataColumn('Time', object, 'timestamp', function(time) { return time.format('LLL'); });
+        html += dataColumn('Changeset', object, 'changeset');
+        html += dataColumn('User', object, 'user');
 
-        html += "<tr><td class='first_column'>Changeset</td>";
-        for (i = 0; i < object.length; i++) {
-            step = object[i];
-            html += "<td>" + step.changeset + "</td>";
-        }
-        html += "</tr>";
-
-        html += "<tr><td class='first_column'>User</td>";
-        for (i = 0; i < object.length; i++) {
-            step = object[i];
-            html += "<td>" + step.user + "</td>";
-        }
-        html += "</tr>";
-
-        html += "<tr><th class='first_column'>Tags</th>";
+        html += "<tr class='row_header'><th class='first_column'>Tags</th>";
         for (i = 0; i < object.length; i++) {
             html += "<th></th>";
         }
@@ -75,13 +85,7 @@ $('#go').click(function() {
         }
 
         for (key in objectTags) {
-            html += "<tr><td class='first_column'>" + key + "</td>";
-            for (i = 0; i < object.length; i++) {
-                step = object[i];
-                tagValue = step.tags[key];
-                html += "<td>" + (tagValue === undefined ? "" : tagValue) + "</td>";
-            }
-            html += "</tr>";
+            html += dataColumn(key, object, 'tags', function(tags) { return (tags[key] === undefined ? "" : tags[key]); });
         }
 
         html += "</table>";
