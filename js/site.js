a0683bc183feda729e0864fd7e83c7de8069ffb0
Tom MacWright <tom@macwright.org>
2013-06-04 12:34:14 -0400

    d3ize

--- js/site.js
+++ js/site.js
@@ -1,53 +1,58 @@
-var hist = require('../index.js');
+var hist = require('../index.js'),
+    d3 = require('d3');
 
 var map = L.map('map').setView([51.505, -0.09], 13);
 L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
 var overlays = L.featureGroup([]).addTo(map);
 
-$(document).ready(function() {
+d3.select(window).on('load', function() {
     var hash = document.location.hash;
     if (hash) {
         var match = /^#\/(node|way|relation)\/(\d*)/.exec(hash);
         if (match) {
-            $('#type').val(match[1]);
-            $('#id').val(match[2]);
-            $('#go').click();
+            d3.select('#type').property('value', match[1]);
+            d3.select('#id').property('value', match[2]);
+            clickGo();
         } else {
             document.location.hash = "";
         }
     }
 });
 
-var dataColumn = function(row_name, data, key, value_transform) {
-    var transform = value_transform || function(keyval) { return keyval; };
-    var prev = null;
+function row(field, title, formatter, tag) {
+    return function(selection) {
+        var prev;
 
-    var html = "<tr><td class='first_column'>" + row_name + "</td>";
-    for (var i = 0; i < data.length; i++) {
-        var val = transform(data[i][key]);
+        selection.append('th').attr('class', 'field').text(title || field);
 
-        var clazz = '';
-        if (prev == val) {
-            clazz = 'unchanged';
-        } else if (!prev && val && val != prev) {
-            clazz = 'added';
-        } else if (prev && !val) {
-            clazz = 'removed';
-        } else if (val && prev != val) {
-            clazz = 'changed';
+        selection
+            .selectAll('td.version')
+            .data(function(d) { return d; })
+            .enter()
+            .append('td')
+            .attr('class', function(d) {
+                return 'version_cell ' + pClass(tag ? d.tags[field] : d[field]);
+            })
+            .html(function(d) {
+                return (formatter || function(x) { return x; })(tag ? d.tags[field] : d[field]);
+            });
+
+        function pClass(val) {
+            try {
+                if (prev == val)  return 'unchanged';
+                else if (!prev && val && val != prev) return 'added';
+                else if (prev && !val) return 'removed';
+                else if (val && prev != val) return 'changed';
+            } finally { prev = val; }
         }
+    };
+}
 
-        html += "<td class='" + clazz + " version_cell'>" + val + "</td>";
-        prev = val;
-    }
-    html += "</tr>";
+d3.select('#go').on('click', clickGo);
 
-    return html;
-};
-
-$('#go').click(function() {
-    var type = $('#type').val(),
-        id = +$('#id').val();
+function clickGo() {
+    var type = d3.select('#type').property('value'),
+        id = +d3.select('#id').property('value');
 
     console.log("Getting history for " + type + " " + id);
     document.location.hash = "/" + type + "/" + id;
@@ -58,20 +63,34 @@ $('#go').click(function() {
             object = objects[type][id],
             html = "<table border='1' style='min-width: " + object.length * 200 + "px;'>";
 
-        console.log(objects);
+        var table = d3.select('#history-2')
+            .append('table')
+            .datum(object);
+
+        table.append('tr').attr('class', 'row_header').call(row('version', 'Version'));
+        table.append('tr').call(row('timestamp', 'Time', timeFormat));
+        table.append('tr').call(row('changeset', 'Changeset'));
+        table.append('tr').call(row('user', 'User', userLink));
+        table.append('tr')
+            .attr('class', 'row_header')
+            .append('th')
+            .attr('colspan', function(d) { return d.length + 1; })
+            .text('Tags');
+
+        object.reduce(function(memo, o) {
+            d3.keys(o.tags).forEach(function(s) { memo.add(s); });
+            return memo;
+        }, d3.set()).forEach(function(tag) {
+            table.append('tr').call(row(tag, tag, null, true));
+        });
+
+        function timeFormat(d) { return d.format('LLL');  }
+
+        function userLink(d) {
+            return '<a target="_blank" href="http://openstreetmap.org/user/' + d + '">' + d + '</a>';
+        }
 
         overlays.clearLayers();
-
-        html += "<tr class='row_header'><th class='first_column'>Version</th>";
-        for (i = 0; i < object.length; i++) {
-            step = object[i];
-            html += "<th>" + step.version + "</th>";
-        }
-        html += "</tr>";
-
-        html += dataColumn('Time', object, 'timestamp', function(time) { return time.format('LLL'); });
-        html += dataColumn('Changeset', object, 'changeset');
-        html += dataColumn('User', object, 'user');
 
         if (type == 'node') {
             for (i = 0; i < object.length; i++) {
@@ -80,32 +99,11 @@ $('#go').click(function() {
                 }
             }
             map.fitBounds(overlays.getBounds());
-            $('#map').show();
+            d3.select('#map').style('display', 'block');
             map.invalidateSize();
         } else {
-            $('#map').hide();
+            d3.select('#map').style('display', 'none');
             map.invalidateSize();
         }
-
-        html += "<tr class='row_header'><th class='first_column'>Tags</th>";
-        for (i = 0; i < object.length; i++) {
-            html += "<th></th>";
-        }
-        html += "</tr>";
-
-        for (i = 0; i < object.length; i++) {
-            for (key in object[i].tags) {
-                if (object[i].tags.hasOwnProperty(key)) {
-                    objectTags[key] = null;
-                }
-            }
-        }
-
-        for (key in objectTags) {
-            html += dataColumn(key, object, 'tags', function(tags) { return (tags[key] === undefined ? "" : tags[key]); });
-        }
-
-        html += "</table>";
-        $('#history').html(html);
     });
-});
+}
